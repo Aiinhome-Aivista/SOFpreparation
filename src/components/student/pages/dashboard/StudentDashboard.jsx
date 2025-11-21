9c8b7f7a0795a8c92c4a0ceb62d63568db66906e
Rohan Das <rohandas8944@gmail.com>
2025-11-21 14:49:07 +0530

    add ui

--- src/components/student/pages/dashboard/StudentDashboard.jsx
+++ src/components/student/pages/dashboard/StudentDashboard.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+// import { Tabs, TabsList, TabsTrigger, TabsContent } from '../../ui/tabs'
+import { BookOpen, Sparkles, Target } from 'lucide-react'
 
-function StudentDashboard() {
+function StudentDashboard({ onStartTest }) {
   return (
-    <div>StudentDashboard</div>
+    <></>
+      // <div className="container mx-auto px-4 py-8">
+      //   <Tabs defaultValue="tests" className="space-y-6">
+          
+      //     {/* Top Tabs */}
+      //     <TabsList className="bg-white shadow-sm border rounded-xl p-1">
+      //       <TabsTrigger value="tests" className="flex items-center gap-2">
+      //         <BookOpen className="size-4" />
+      //         My Tests
+      //       </TabsTrigger>
+
+      //       <TabsTrigger value="smart" className="flex items-center gap-2">
+      //         <Sparkles className="size-4" />
+      //         Smart Study
+      //       </TabsTrigger>
+
+      //       <TabsTrigger value="practice" className="flex items-center gap-2">
+      //         <Target className="size-4" />
+      //         Self Practice
+      //       </TabsTrigger>
+      //     </TabsList>
+
+      //     {/* TAB CONTENTS */}
+      //     <TabsContent value="tests">
+      //       <MyTests onStartTest={onStartTest} />
+      //     </TabsContent>
+
+      //     <TabsContent value="smart">
+      //       <SmartStudy />
+      //     </TabsContent>
+
+      //     <TabsContent value="practice">
+      //       <SelfPractice onStartTest={onStartTest} />
+      //     </TabsContent>
+
+      //   </Tabs>
+      // </div>
   )
 }
 
